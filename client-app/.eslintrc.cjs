90d05721765109350fc4463c0a692d3172df68a2
smsabir <sabir.smaa2@gmail.com>
2023-11-24 02:45:07 +0200

    End of Section 5

--- client-app/.eslintrc.cjs
+++ client-app/.eslintrc.cjs
@@ -1,18 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {"browser": true,
+  "es2020": true,
+  "jest": true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+        "airbnb-typescript",
+        "plugin:import/typescript"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-explicit-any": "error"
   },
 }
